6e41f624925e589d4eedb6aee1d6fd9a82a4c2c4
tiantangyu612 <lizhitao123@126.com>
2018-01-11 23:41:27 +0800

    优化代码

---
+++ monitor-datahub/src/main/java/monitor/datahub/MonitorDataHubFactory.java
@@ -0,0 +1,9 @@
+package monitor.datahub;
+
+/**
+ * Created by lizhitao on 2018/1/11.
+ * MonitorDataHubFactory
+ */
+public interface MonitorDataHubFactory {
+    MonitorDataHub createMonitorDataHub();
+}
---
+++ monitor-datahub/src/main/java/monitor/datahub/SocketDataHub.java
@@ -0,0 +1,12 @@
+package monitor.datahub;
+
+/**
+ * Created by lizhitao on 2018/1/11.
+ * 使用 Socket 收集采集器采集的数据
+ */
+public class SocketDataHub implements MonitorDataHub {
+    @Override
+    public void collectData(String collectData) {
+
+    }
+}
