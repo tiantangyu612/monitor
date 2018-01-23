058f12b3117adb5013c49acda314f0470f9b4ed1
tiantangyu612 <lizhitao123@126.com>
2018-01-23 22:13:27 +0800

    应用dao

---
+++ monitor-view/src/main/java/monitor/view/dao/mysql/ApplicationDao.java
@@ -0,0 +1,69 @@
+package monitor.view.dao.mysql;
+
+import monitor.view.domain.entity.Application;
+import monitor.view.domain.entity.Product;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * Created by lizhitao on 2018/1/23.
+ * 应用操作 dao
+ */
+public interface ApplicationDao {
+    /**
+     * 获取单个应用信息
+     *
+     * @param id
+     * @return
+     */
+    @Select("SELECT * FROM Application where id = #{id}")
+    Application getById(Integer id);
+
+    /**
+     * 查询应用总数
+     *
+     * @return
+     */
+    @Select("SELECT COUNT(*) FROM Application where productId=?")
+    int count(@Param("productId") Integer productId);
+
+    /**
+     * 插入应用信息
+     *
+     * @param application
+     * @return
+     */
+    @Insert("INSERT INTO Application (id, name, productId, description, owner, createTime) VALUES(#{id},#{name}, #{productId}, #{description}, #{owner}, #{createTime})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(Application application);
+
+    /**
+     * 获取应用列表
+     *
+     * @param productId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    @Select("SELECT * FROM Application WHERE productId=? ORDER BY id DESC LIMIT #{limit} OFFSET #{offset}")
+    List<Product> getProductList(@Param("productId") Integer productId, @Param("offset") Integer offset, @Param("limit") Integer limit);
+
+    /**
+     * 更新应用信息
+     *
+     * @param application
+     * @return
+     */
+    @Update("UPDATE Application SET name=#{name},productId=#{productId}, owner=#{owner},description=#{description} WHERE id=#{id}")
+    int updateById(Application application);
+
+    /**
+     * 删除应用信息
+     *
+     * @param id
+     * @return
+     */
+    @Update("DELETE FROM Application WHERE id=#{id}")
+    int deleteById(Integer id);
+}
